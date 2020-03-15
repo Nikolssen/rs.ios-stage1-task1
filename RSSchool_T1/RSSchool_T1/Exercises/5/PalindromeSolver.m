3368803490e0e36e13762b18f9c2c1cfe2d7e1e8
Ivan Budovich <56168159+Nikolssen@users.noreply.github.com>
2020-03-15 08:32:13 +0300

    feat: check task5 for impossibility

--- RSSchool_T1/RSSchool_T1/Exercises/5/PalindromeSolver.m
+++ RSSchool_T1/RSSchool_T1/Exercises/5/PalindromeSolver.m
@@ -4,7 +4,40 @@
 
 // Complete the highestValuePalindrome function below.
 - (NSString *) highestValuePalindrome:(NSString *)s n:(NSNumber *)n k:(NSNumber *)k {
-    return @"";
+    int minPossibleChanges = 0;
+    NSString *result = [s copy];
+    for (int i=0; i<[n intValue] /2; i++) {
+        if ([s characterAtIndex:i] !=[s characterAtIndex:([n intValue]-i-1)])    minPossibleChanges++;
+    }
+    if (minPossibleChanges > [k intValue])
+    result = @"-1";
+        else
+        {
+            
+            if ([k intValue] == minPossibleChanges)
+                {
+                    for (int i=0; i<[n intValue] /2; i++)
+                        {
+                            if ([s characterAtIndex:i] > [s characterAtIndex:([n intValue]-i-1)])
+                                {
+                                    NSString *curChar = [NSString stringWithFormat:@"%c" , [s characterAtIndex:([n intValue]-i-1)]];
+                                    result = [result stringByReplacingCharactersInRange: NSMakeRange(i, 1) withString: curChar];
+                        
+                                }
+                            else if ([s characterAtIndex:i] > [s characterAtIndex:([n intValue]-i-1)])
+                                {
+                                    NSString *curChar = [NSString stringWithFormat:@"%c" , [s characterAtIndex:i]];
+                                    result = [result stringByReplacingCharactersInRange: NSMakeRange((([n intValue]-i-1)), 1) withString: curChar];
+                                }
+                
+                        }
+                
+                }
+            
+            
+    
+        }
+ return result;
 }
 
 @end
