94301d9c4528db199aa2506b97f1799c063db115
Ivan Budovich <56168159+Nikolssen@users.noreply.github.com>
2020-03-15 08:30:21 +0300

    Add files via upload

--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -4,7 +4,14 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
-    return @"";
+    int summ=0;
+    for (NSNumber *dishPrice in bill) {
+        summ+= [dishPrice intValue];
+    }
+    if ((summ - [bill[index] intValue])/2 == [sum intValue]) {return @"Bon Appetit";}
+    else
+        return [NSString stringWithFormat:@"%i", ([sum intValue] - ((summ - [bill[index] intValue])/2))];
+   
 }
 
 @end
