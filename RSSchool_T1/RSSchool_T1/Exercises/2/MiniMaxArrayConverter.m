7c10fa65306be83ccb184186853ed19cc65682b7
Ivan Budovich <56168159+Nikolssen@users.noreply.github.com>
2020-03-15 08:29:31 +0300

    feat: solve Task2

--- RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
@@ -4,7 +4,14 @@
 
 // Complete the convertFromArray function below.
 - (NSArray<NSNumber*>*)convertFromArray:(NSArray<NSNumber*>*)array {
-    return @[@0];
+    NSArray<NSNumber*> *sortArray = [array sortedArrayUsingSelector:@selector(compare:)];
+    int min = 0;
+    int max =0;
+    for (int i = 0; i<[sortArray count]-1;i++) {min += [sortArray[i] intValue];}
+    for (int i= 1; i<[sortArray count];i++) {max+=[sortArray[i] intValue];}
+    NSNumber *resMin = [NSNumber numberWithInt:min];
+    NSNumber *resMax = [NSNumber numberWithInt:max];
+    return @[resMin, resMax];
 }
 
 @end
