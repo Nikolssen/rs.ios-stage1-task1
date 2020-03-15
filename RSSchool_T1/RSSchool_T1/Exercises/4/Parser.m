b1e7cbc7a75d37fb60e1f19a5b1b3fa79fbd6de4
Ivan Budovich <56168159+Nikolssen@users.noreply.github.com>
2020-03-15 08:31:01 +0300

    feat: solve Task4

--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -4,7 +4,80 @@
 
 // Complete the parseString function below.
 - (NSArray <NSString*>*)parseString:(NSString*)string {
-    return @[@"Test"];
+    NSMutableArray <NSString*> *resultArray = [NSMutableArray new];
+    int count;
+    int beg;
+    int j;
+    int i = 0;
+    while (i<[string length]-1) {
+        if ([string characterAtIndex:i] == '<') {
+            BOOL flag = YES;
+            BOOL endOfLine = NO;
+            beg = i+1;
+            j = i+1;
+            count = 0;
+            while (flag &&(!endOfLine)){
+                if ([string characterAtIndex:j] == '<') {count++;}
+                else if ([string characterAtIndex:j] == '>')
+            {
+                    if (count > 0 ) {count--;}
+                    else {flag = NO; }
+            }
+                         j++;
+                if  (j == [string length]) endOfLine = YES;
+                         }
+            
+            
+            if (!flag) [resultArray addObject: [string substringWithRange:NSMakeRange(beg, j-1-beg)]];
+            
+        }
+        else if ([string characterAtIndex:i] == '[') {
+                       BOOL flag = YES;
+                       BOOL endOfLine = NO;
+                       beg = i+1;
+                       j = i+1;
+                       count = 0;
+                       while (flag && (!endOfLine)) {
+                           if ([string characterAtIndex:j] == '[') {count++;}
+                           else if ([string characterAtIndex:j] == ']')
+                       {
+                               if (count > 0 ) {count--;}
+                               else {flag = NO; }
+                       }
+                                    j++;
+                           if  (j == [string length]) endOfLine = YES;
+                                    }
+                       
+                       
+                       if (!flag) [resultArray addObject: [string substringWithRange:NSMakeRange(beg, j-1-beg)]];
+                  
+               }
+        
+        else if ([string characterAtIndex:i] == '(') {
+                     BOOL flag = YES;
+                     BOOL endOfLine = NO;
+                     beg = i+1;
+                     j = i+1;
+                     count = 0;
+                     while (flag && (!endOfLine)) {
+                         if ([string characterAtIndex:j] == '(') {count++;}
+                         else if ([string characterAtIndex:j] == ')')
+                     {
+                             if (count > 0 ) {count--;}
+                             else {flag = NO; }
+                     }
+                                  j++;
+                         if  (j == [string length]) endOfLine = YES;
+                                  }
+                     
+                     
+            if (!flag) [resultArray addObject: [string substringWithRange:NSMakeRange(beg, j-1-beg)]];}
+                    
+            
+        i++;
+        
+    }
+    return resultArray;
 }
-
 @end
+        
