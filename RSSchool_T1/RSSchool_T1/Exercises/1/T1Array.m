bdb807062e78d9f839bdb4a76274d82e408253af
Ivan Budovich <56168159+Nikolssen@users.noreply.github.com>
2020-03-15 08:28:20 +0300

    feat: solve Task1

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -4,7 +4,21 @@
 
 // Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+    NSMutableArray *resultArray = [sadArray mutableCopy];
+    int i = 1;
+    if ([resultArray count] >0){
+    while (i<[resultArray count]-1)
+    {
+      
+        
+        if ([resultArray[i] intValue]>[resultArray[i-1] intValue] + [resultArray[i+1] intValue])
+            {[resultArray removeObjectAtIndex:i];
+                if (i!=1) {i--;}}
+        else
+            i++;
+    }}
+    return resultArray;
+    
 }
 
 @end
